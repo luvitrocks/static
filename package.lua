758361ee19c6d016869b1e93aa09f4344f9af2e8
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-07-22 21:49:27 +0300

    add package.lua

---
+++ package.lua
@@ -0,0 +1,15 @@
+-- for older versions of luvit and alternative package managers
+return {
+	name = "luvit-static",
+	version = "0.0.1",
+	description = "Serve static files with luvit.io and Utopia",
+	repository = {
+		url = "http://github.com/luvitrocks/luvit-static.git",
+	},
+	author = {
+		name = "Dmitri Voronianski",
+		email = "voronianski@gmail.com"
+	},
+	licenses = {"MIT"},
+	main = 'init.lua'
+}
